8dbbec96ba329886b5cac6927026e32a5ba33e71
Mario Segal <mario.segal@gmail.com>
2014-09-08 12:58:24 -0400

    9/8/2014

--- mtcars analysis.R
+++ mtcars analysis.R
@@ -12,7 +12,9 @@ ggplot(data,aes(y=mpg,x=am))+geom_violin(aes(color=am))+geom_boxplot(aes(fill=am
   ggtitle("MPG by Transmission Type")
 
 data1 <- melt(data,id.var=c("car","am","mpg"))
-ggplot(data1,aes(y=mpg,x=value,color=am))+geom_point()+stat_smooth(se=F,method="lm")+
+ggplot(data1,aes(y=mpg,x=value,color=am))+geom_point()+
+  stat_smooth(formula=y~poly(x,degree=1),se=F,method="lm",linetype=1)+
+  stat_smooth(formula=y~poly(x,degree=2),se=F,method="lm",linetype=2)+
   facet_wrap(~variable,scales="free")+theme_bw()+theme(legend.position="bottom")+
   ggtitle("MPG versus Other Variables by Type of Transmission")+scale_y_continuous("MPG")+
   scale_x_continuous("Variable Value")+scale_color_discrete("Transmission\nType")
@@ -44,10 +46,52 @@ model2 <- train(mpg~.,data[-12],method="lmStepAIC",direction="backward",trace=F)
 model3 <- train(mpg~.,data[-12],method="lmStepAIC",direction="forward")
 model4 <- train(mpg~.,data[-12],method="lmStepAIC")
 
-summary(model2)
+fit2 <- lm(mpg~wt + qsec + am,data=data[-12])
+fit2 <- summary(model2)
 summary(model3)
 summary(model4)
 
-#try some interactions
+fit2x <- lm(mpg~qsec+wt+am,data=,mtcars)
 
+#try diagnostic plots
+diags <- data.frame(car=row.names(mtcars),residuals=fit2$residuals,am=mtcars$am,wt=mtcars$wt,qsec=mtcars$qsec,
+                    fitted=predict(model2,data))
+diags1 <- melt(diags,id.vars=c("car","residuals")) 
+ggplot(diags1,aes(y=residuals,x=value,color=variable))+geom_point()+
+  facet_wrap(~variable,scales="free_x")+theme_bw()+theme(legend.position="none")+
+  ggtitle("Diagnostic Plots for Final Model")+scale_x_continuous("Variable")
+
+round(cov(diags[-1])[1,-1],4)
+
+#how to do my own qq plot
+
+autoplot.lm(model2)
+#2 and 4 worked well, same model - 83% R2 whihc is great
+
+#try some interactions
+y <- data$mpg
+x <- mtcars[-c(1)]
+z <- data.frame(extra=numeric(32))
+k=1
+for (i in 1:dim(x)[2]) {
+  for (j in 1:dim(x)[2]) {
+    k=k+1
+    z <- data.frame(z,x[,i]*x[,j])
+    names(z)[k] <- paste(names(x)[i],names(x)[j],sep=".")
+  }
+}
+z1 <- z[-1]
+row.names(x) <- row.names(z1) <- row.names(y)
+modeldata <- cbind(y,x,z1)
+model5 <- train(y~.,data=modeldata,method="lmStepAIC",direction="backward",trace=F)
+train(y~(cyl+disp+hp+drat+wt+qsec+vs+am+gear+carb)^2,data=mtcars,method="lm")
+
+which
+
+#not working
+
+warnings()
 # do some diagnositc plots that are needed
+
+autoplot.lm(fit2,which=(1,6))
+
